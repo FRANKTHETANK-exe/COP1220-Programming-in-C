f3c2ae3139f3419ea767f504c9047de958db87df
FRANKTHETANK-exe <frankeacosta05@gmail.com>
2025-01-18 18:29:06 -0500

    Add files via upload

---
+++ assignment 1-2.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main() {
+    // Unit Test #1 Lyrics
+    printf("Oh, well imagine /\n");
+    printf("As I'm pacing the pews in a church corridor /\n");
+    printf("And I can't help but to hear /\n");
+    printf("No, I can't help but to hear an exchanging of words...\n\n");
+
+    // Unit Test #2 Lyrics
+    printf("Oh-oh-oh-oh-oh,\n\n");
+    printf("oh-oh-oh-oh,\n\n");
+    printf("oh-oh-oh /\n\n");
+    printf("Caught in a bad romance...\n\n");
+
+    // Unit Test #3 Lyrics
+    printf("Coming out of my cage\n");
+    printf("And I've been doing just fine\n");
+    printf("Gotta gotta be down\n");
+    printf("Because I want it all\n\n");
+
+    // Unit Test #4 Lyrics
+    printf("Now, this is a story all about how\n");
+    printf("My life got flipped-turned upside down\n");
+    printf("And I'd like to take a minute\n");
+    printf("Just sit right there\n");
+    printf("I'll tell you how I became the prince of a town called Bel-Air\n");
+
+    return 0;
+}
